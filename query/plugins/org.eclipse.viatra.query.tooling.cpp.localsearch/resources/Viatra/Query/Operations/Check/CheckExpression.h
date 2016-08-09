9a58c2dac2f3526022248bb4eac6a4718aa46459
jogaproger <jogaproger@gmail.com>
2016-08-09 13:04:53 +0200

    rename to CheckExpression

--- query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Operations/Check/CheckExpression.h
+++ query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Operations/Check/CheckExpression.h
@@ -7,8 +7,12 @@
  *
  * Contributors:
  *     Robert Doczi - initial API and implementation
+ *     Gabor Szilagyi - Additional features implementation
+ *     Krisztian Toth - Additional features implementation
  *******************************************************************************/
 #pragma once
+
+#include <tuple>
 
 #include "CheckOperation.h"
 
@@ -18,41 +22,62 @@ namespace Operations {
 namespace Check {
 
 /**
- * @brief Expression check for running simple expressions.
+ * @brief This CheckOperation describes a custom check
  *
- * This class handles running 'check(...expression...)' constraints on the model. This is done
- * via helper check classes containing the expression itself.
- *
- * @tparam Check The type of the helper check class.
  * @tparam MatchingFrame Describes the structure of the *MatchingFrame* the operation is executed on.
- */
-template<class Check, class MatchingFrame>
-class CheckExpression: public CheckOperation<MatchingFrame> {
-public:
-    /**
-     * Creates an instance of an expression check using the specified check.
-     *
-     * @param check An instance of the class containing the proper check.
-     */
-    CheckExpression(Check check);
+ * @tparam T The arbitrary list of member pointers to access the proper field of the frame (to pass them to the matcher).
+*/
+template<class MatchingFrame, class ...T>
+class CheckExpression : public CheckOperation<MatchingFrame> {
+private:
+	// Helper struct, so simple variadic type can be expanded into member pointer list
+	template<typename CLASS, typename FIELD> 
+	struct ToMemberPointer {
+		using type = FIELD CLASS::*;		
+	};
+	
 
-    bool check(MatchingFrame& frame, const Matcher::ISearchContext& context);
+	std::tuple<typename ToMemberPointer<MatchingFrame, T>...> _memberPointers;
+	std::function<bool(T...)> _checkerFunction;
+	
+	// Helper method for tuple
+	template<size_t... index>
+	bool invoke_helper(MatchingFrame& frame, std::index_sequence<index...>);
+
+	
+public:
+
+	CheckExpression(std::function<bool(T...)> checkerFunction, typename ToMemberPointer<MatchingFrame, T>... memberPointers )
+		: _memberPointers(memberPointers...)
+		, _checkerFunction(checkerFunction)
+	{}
+
+protected:
+	bool check(MatchingFrame& frame, const Matcher::ISearchContext& context);
 
 private:
-    Check _check;
+
 };
 
-template<class Check, class MatchingFrame>
-inline CheckExpression<Check, MatchingFrame>::CheckExpression(Check check) :
-        _check(check) {
+template<class MatchingFrame, class ...T>
+inline bool CheckExpression<MatchingFrame, T...>::check(MatchingFrame & frame, const Matcher::ISearchContext & context) {
+	constexpr auto Size = std::tuple_size<typename std::decay<std::tuple<T...>>::type>::value;
+	return invoke_helper(frame, std::make_index_sequence<Size>{});
 }
 
-template<class Check, class MatchingFrame>
-inline bool CheckExpression<Check, MatchingFrame>::check(MatchingFrame& frame, const Matcher::ISearchContext& context) {
-    return _check(frame);
+template<class MatchingFrame, class ...T>
+CheckExpression<MatchingFrame, T...>* create_CheckExpression( std::function<bool(T...)> checkerFunction, typename ToMemberPointer<MatchingFrame, T>... memberPointers) {
+	return new CheckExpression<MatchingFrame, T...>(checkerFunction, memberPointers...);
+}
+
+template<class MatchingFrame, class ...T>
+template<size_t ...index>
+inline bool CheckExpression<MatchingFrame, T...>::invoke_helper(MatchingFrame& frame, std::index_sequence<index...>) {
+	return _checkerFunction((frame.*std::get<index>(std::forward<std::tuple<typename ToMemberPointer<MatchingFrame, T>...>>(_memberPointers)))... );
+
 }
 
 }  /* namespace Check */
-}  /* namespace Operations */
+}  /* namespace Util */
 }  /* namespace Query */
 }  /* namespace Viatra */
