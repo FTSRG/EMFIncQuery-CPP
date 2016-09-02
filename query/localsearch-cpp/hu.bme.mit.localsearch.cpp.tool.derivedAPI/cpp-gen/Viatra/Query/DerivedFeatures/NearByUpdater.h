d45246d1441ca57df1d4446988658c42f249d866
toth-krisztian <tht.krisztian@gmail.com>
2016-09-02 12:14:41 +0200

    Ready to implement updater API code generation

--- query/localsearch-cpp/hu.bme.mit.localsearch.cpp.tool.derivedAPI/cpp-gen/Viatra/Query/DerivedFeatures/NearByUpdater.h
+++ query/localsearch-cpp/hu.bme.mit.localsearch.cpp.tool.derivedAPI/cpp-gen/Viatra/Query/DerivedFeatures/NearByUpdater.h
@@ -1,6 +1,10 @@
 #ifndef VIATRA__QUERY__DERIVED_FEATURES__NEAR_BY_UPDATER_H_
 #define VIATRA__QUERY__DERIVED_FEATURES__NEAR_BY_UPDATER_H_
 
+#include <vector>
+#include <stdexcept>
+#include <algorithm>
+
 #include "Viatra/Query/QueryEngine.h"
 #include "Viatra/Query/Matcher/ModelIndex.h"
 
@@ -35,44 +39,49 @@ namespace Viatra {
 namespace Viatra {
 namespace Query {
 namespace DerivedFeatures {
+
+	template<class ModelRoot>
+	struct NearByUpdate{
+		static void update(ModelRoot modelRoot, int robotPartID, int trainID, double robX, double robY, double robZ, double trX, double trY, double trZ){
+			/*
+			 * Critical Section START
+			 * Atomicity is mandatory
+			 * Not supported parallel modifications and queries
+			 */
+			auto srcInstanceList = ModelIndex<typename std::remove_pointer< ::RailRoadModel::RobotPart >::type, ModelRoot>::instances(&modelRoot);
+			auto srcIDPredicate = [robotPartID](const ::RailRoadModel::RobotPart* src){
+				return src->id == robotPartID;
+			};
+			
+			auto srcObj = std::find_if(srcInstanceList.begin(), srcInstanceList.end(), srcIDPredicate);
+			
+			if(srcObj == srcInstanceList.end()) throw new std::invalid_argument("Source ID not found");
+			
+			auto trgInstanceList = ModelIndex<typename std::remove_pointer< ::RailRoadModel::Train >::type, ModelRoot>::instances(&modelRoot);
+			auto trgIDPredicate = [trainID](const ::RailRoadModel::Train* trg){
+				return trg->id == trainID;
+			};
+			
+			auto trgObj = std::find_if(trgInstanceList.begin(), trgInstanceList.end(), trgIDPredicate);
+			
+			if(trgObj == trgInstanceList.end()) throw new std::invalid_argument("Target ID not found");
+			
+			auto engine = QueryEngine<ModelRoot>::of(&modelRoot);
+			auto nearByMatcher = engine.template matcher< NearByQuerySpecification >();
+			auto matches = nearByMatcher.matches(robotPartID, trainID, robX, robY, robZ, trX, trY, trZ);
+			
+			auto tempTrg = std::find_if((*srcObj)->nearBy.begin(), (*srcObj)->nearBy.end(), trgIDPredicate);
+			
+			if(matches.size() > 0){	if(tempTrg == (*srcObj)->nearBy.end()) (*srcObj)->nearBy.push_back(*trgObj);}
+			else if(tempTrg != (*srcObj)->nearBy.end()) (*srcObj)->nearBy.erase(tempTrg);
+			
+			/*
+			* Critical Section END
+			*/
+		}
+	};
 } /* namespace DerivedFeatures */
 } /* namespace Query */
 } /* namespace Viatra */
-
-using namespace ::Viatra::Query;
-
-template<class ModelRoot>
-static void update(int robotPartID, int trainID, double robX, double robY, double robZ, double trX, double trY, double trZ){
-	auto comparator = [](const ::RailRoadModel::RobotPart* a,const ::RailRoadModel::RobotPart* b){ return a->id < b->id;};
-  std::map<::RailRoadModel::RobotPart*, std::vector<::RailRoadModel::Train*>,
-    std::function<bool(const ::RailRoadModel::RobotPart* a,const ::RailRoadModel::RobotPart* b)>>
-      derivedKnowledge(comparator);
-	/*
-	 * Critical Section START
-	 * Atomicity is mandatory
-	 * Not supported parallel modifications and queries
-	 */
-	ModelRoot modelRoot;
-	auto instanceList = ModelIndex<typename std::remove_pointer<::RailRoadModel::RobotPart>::type, ModelRoot>::instances(&modelRoot);
-	for(auto& src : instanceList){
-		src->nearBy.clear();
-		derivedKnowledge[src];
-	}
-
-	QueryEngine<ModelRoot> engine = QueryEngine<ModelRoot>::empty();
-	DerivedFeatures::NearByMatcher<ModelRoot> nearByMatcher = engine.matcher<DerivedFeatures::NearByQuerySpecification>();
-	auto matches = nearByMatcher.matches(robotPartID, trainID, robX, robY, robZ, trX, trY, trZ);
-
-	for(auto match : matches) derivedKnowledge[match.robotPart].push_back(match.train);
-	for(auto& src : instanceList){
-		 auto srcMapIterator = derivedKnowledge.find(src);
-		 if(srcMapIterator != derivedKnowledge.end()){
-			 for(auto& trg : srcMapIterator->second) src->nearBy.push_back(trg);
-		 }
-	}
-	/*
-	* Critical Section END
-	*/
-}
 
 #endif /*  VIATRA__QUERY__DERIVED_FEATURES__NEAR_BY_UPDATER_H_ */
--- query/localsearch-cpp/hu.bme.mit.localsearch.cpp.tool.derivedAPI/cpp-gen/main.cpp
+++ query/localsearch-cpp/hu.bme.mit.localsearch.cpp.tool.derivedAPI/cpp-gen/main.cpp
@@ -19,12 +19,43 @@ using namespace std;
 int main(){
   ModelRoot modelRoot;
 
-	//auto engine = QueryEngine<ModelRoot>::of(&modelRoot);
+  auto t1 = new ::RailRoadModel::Train(); t1->id = 1;
+  auto t2 = new ::RailRoadModel::Train(); t2->id = 2;
+  auto t3 = new ::RailRoadModel::Train(); t3->id = 3;
+  auto r1 = new ::RailRoadModel::RobotPart(); r1->id = 4;
+  auto r2 = new ::RailRoadModel::RobotPart(); r2->id = 5;
+  auto r3 = new ::RailRoadModel::RobotPart(); r3->id = 6;
+
+  //DEBUG
+  for(auto& src : ::RailRoadModel::RobotPart::_instances){
+    //DEBUG
+    cout << "Source ID: " << src->id << endl;
+    for(auto& trg : src->nearBy) cout << "Target ID: " << trg->id << endl;
+  }
   cout << "Engine Created" << endl;
-  //auto nearByMatcher = engine.matcher<NearByQuerySpecification>();
   cout << "Matcher Created" << endl;
-  int robotPartID = 0; int trainID=0; double robX=0.0; double robY=0.0; double robZ=0.0; double trX=0.0; double trY=0.0; double trZ=0.0;
-  //auto matches = nearByMatcher.matches(robotPartID, trainID, robX, robY, robZ, trX, trY, trZ);
-	update<ModelRoot>(robotPartID, trainID, robX, robY, robZ, trX, trY, trZ);
-	cout << "Matches Queried" << endl;
+
+  double robX=0.0; double robY=0.0; double robZ=0.0;
+  double trX=0.0; double trY=0.0; double trZ=0.0;
+
+  int i = 0;
+  while(i < 6){
+  	for(int j = 4; j <= 6; j++){ //RobotParts
+      //cout << "RobotPart ID: " << j << "(" <<robX <<", " << robY << ", " << robZ << ")" << endl;
+      for(int k = 1; k <= 3; k++){ //Trains
+        NearByUpdate<ModelRoot>::update(modelRoot,j, k, robX, robY, robZ, trX, trY, trZ);
+        //cout << "\tTrain ID: " << k << "(" <<trX <<", " << trY << ", " << trZ << ")" << endl;
+      }
+    }
+  	cout << endl << "!!!!Updated" << endl;
+    cout << "Objects status:" << endl;
+
+    for(auto& src : ::RailRoadModel::RobotPart::_instances){
+      cout << "Source ID: " << src->id << "(" <<robX <<", " << robY << ", " << robZ << ")" << endl;
+      for(auto& trg : src->nearBy) cout << "\tTarget ID: " << trg->id << "(" <<trX <<", " << trY << ", " << trZ << ")" << endl;
+    }
+    i++;
+    robX--; //robY--; robZ--;
+    trX++; //trY++; trZ++;
+  }
 }
