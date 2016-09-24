c60fe486f7a406199d58a33af46333ad75e041f5
jogaproger <jogaproger@gmail.com>
2016-09-24 22:48:54 +0200

    destructor fix

--- query/localsearch-cpp/hu.bme.mit.localsearch.cpp.tool.cpsTest/src/DistributedTest/Viatra/Query/Model/ModelElement.h
+++ query/localsearch-cpp/hu.bme.mit.localsearch.cpp.tool.cpsTest/src/DistributedTest/Viatra/Query/Model/ModelElement.h
@@ -17,6 +17,9 @@ namespace Viatra {
 				bool present() { return _present; }
 				id_t id() { return _id; }
 
+				virtual ~ModelElement()
+				{}
+
 			protected:
 				id_t _id;
 				bool _present;
@@ -25,8 +28,6 @@ namespace Viatra {
 					, _present(present)
 				{}
 
-				virtual ~ModelElement()
-				{}
 
 			};
 		}
