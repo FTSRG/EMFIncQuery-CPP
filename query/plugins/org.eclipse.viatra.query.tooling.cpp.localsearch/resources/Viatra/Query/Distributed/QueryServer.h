6f2fa08135d947f7dde9357e3624e1292302ddf4
jogaproger <jogaproger@gmail.com>
2016-10-02 18:14:48 +0200

    QueryServer reborn

---
+++ query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Distributed/QueryServer.h
@@ -0,0 +1,36 @@
+
+#ifndef _VIATRA_QUERY_DISTRIBUTED_QUERYSERVER_
+#define _VIATRA_QUERY_DISTRIBUTED_QUERYSERVER_
+
+#include "network.h"
+
+
+#ifdef _VIATRA_HEADER_ONLY_
+#define VIATRA_FUNCTION inline
+#else
+#define VIATRA_FUNCTION
+#endif
+#define VIATRA_INLINE_FUNCTION inline
+
+namespace Viatra {
+	namespace Query {
+		namespace Distributed {
+
+			class QueryServer : Network::Server
+			{
+				VIATRA_FUNCTION QueryServer(uint16_t port);
+				VIATRA_FUNCTION ~QueryServer();
+			protected:
+				VIATRA_FUNCTION virtual void accept_connection(Network::Connection * c) override;
+				VIATRA_FUNCTION virtual void process_message(Network::Connection * c, Network::byte * bytes, int len)override;
+			};
+
+		}
+	}
+}
+
+#ifdef _VIAATRA_HEADER_ONLY_
+#include"QueryServer.cpp"
+#endif
+
+#endif
