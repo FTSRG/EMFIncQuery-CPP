dfdf468f94804edf2b3485e3bc18d92fcccdf3d9
jogaproger <jogaproger@gmail.com>
2016-10-04 23:54:57 +0200

    ID Generator reconfigurate and default setting

--- query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Distributed/IDGenerator.h
+++ query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Distributed/IDGenerator.h
@@ -13,9 +13,18 @@ namespace Viatra {
 				int64_t increment, nextID;
 				std::mutex id_mutex;
 			public:
-				IDGenerator(int64_t firstID, int64_t increment)
+				IDGenerator(int64_t firstID = 0, int64_t increment = 1)
+					: nextID(firstID)
+					, increment(increment)
 				{
 					nextID = firstID;
+				}
+
+				void reconfigurate(int64_t firstID, int64_t increment)
+				{
+					std::unique_lock<std::mutex> lock(id_mutex);
+					this->nextID = firstID;
+					this->increment = increment;
 				}
 
 				int64_t generate()
