b4d5e38f0511144949c7dec51a56284f0898193d
jogaproger <jogaproger@gmail.com>
2016-10-04 13:56:38 +0200

    little changes

--- query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Distributed/QueryTask.h
+++ query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Distributed/QueryTask.h
@@ -3,6 +3,7 @@
 #define _VIATRA_QUERY_DISTRIBUTED_QUERYTASK_H__
 
 #include"QueryResultCollector.h"
+#include"../Util/HierarchicalID.h"
 
 namespace Viatra {
 	namespace Query {
@@ -10,26 +11,30 @@ namespace Viatra {
 
 			template<typename QuerySpecification>
 			struct QueryTask {
+				
+				// Unique Id for the task inside a QuerySession
+				Util::HierarchicalID<int> id;
+
 				// Index of the body to run on
 				int bodyIndex;	
 				int operationIndex;
 				
 				std::string EncodedMatchingFrame;
 				QueryResultCollector collector;
-				/*
+
 				template<typename MatchingFrame>
-				QueryTask(	const MatchingFrame &f, 
+				QueryTask(	const MatchingFrame& frame, 
 							int bodyIndex, 
 							int operationIndex,
-							QueryResultCollector<QuerySpecification> collector)
+							QueryResultCollector collector)
 					: bodyIndex(bodyIndex)
 					, operationIndex(operationIndex)
 					, EncodedMatchingFrame(frame.SerializeAsString())
 					, collector(collector)
 					, bodyIndex (bodyIndex)
-				{}*/
+				{}
 
-				QueryTask(QueryTask &&) = default;
+				QueryTask(QueryTask<QuerySpecification> &&) = default;
 
 			};
 		}
---
+++ query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Util/HierarchicalID.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define BUFFER_SIZE 20
+
+namespace Viatra {
+	namespace Query {
+		namespace Util {
+
+			template<typename T>
+			using HierarchicalID = std::vector<T>;
+
+		}
+	}
+}
