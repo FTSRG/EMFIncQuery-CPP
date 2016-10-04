bf5967f726ce7527733b38617286b7dc5dbdc0c9
jogaproger <jogaproger@gmail.com>
2016-10-04 23:56:40 +0200

    Hieararchical ID basic implementation

--- query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Util/HierarchicalID.h
+++ query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Util/HierarchicalID.h
@@ -2,13 +2,39 @@
 
 #define BUFFER_SIZE 20
 
+#include<vector>
+
 namespace Viatra {
 	namespace Query {
 		namespace Util {
 
 			template<typename T>
-			using HierarchicalID = std::vector<T>;
+			class HierarchicalID : private std::vector<T>
+			{
+				using std::vector<T>::begin;
+				using std::vector<T>::end;
+			};
 
 		}
 	}
+}
+
+namespace std {
+
+	template<typename T> 
+	struct hash<::Viatra::Query::Util::HierarchicalID<T>> 
+	{
+		size_t operator()(const ::Viatra::Query::Util::HierarchicalID<T>& hID) const 
+		{
+			static std::hash<T> hash;
+			size_t h = 0;
+			for (auto && x : hID)
+			{
+				h *= 31; 
+				h += hash(x);
+			}
+			return h;
+		}
+	};
+
 }
