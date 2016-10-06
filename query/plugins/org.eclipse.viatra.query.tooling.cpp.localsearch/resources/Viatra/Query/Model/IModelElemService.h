800787772ca3ca1f3aa02ad04a578d8bf1f3c21e
jogaproger <jogaproger@gmail.com>
2016-10-06 13:30:22 +0200

    IModelElemService concept art

--- query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Model/IModelElemService.h
+++ query/plugins/org.eclipse.viatra.query.tooling.cpp.localsearch/resources/Viatra/Query/Model/IModelElemService.h
@@ -8,12 +8,57 @@ namespace Viatra {
 			class IModelElemService
 			{
 			public:
-				virtual ~IModelElemService()
-				{}
+				IModelElemService() {}
+				virtual ~IModelElemService() {}
 				/*
-				virtual  getData(int id, int FieldID,) = 0;
+				template<typename T> void setReference( int id, int fieldID, T* reference ){
+					
+				}*/
+
+			protected:
+				virtual std::string getData(int id, int FieldID) = 0;
 				virtual void setData(int id, int FieldID, std::string ser_data) = 0;
-				*/
+				/*
+				// Serialize datatype
+				template<typename T>
+				static std::string serializeAttributeToString(T data)
+				{
+					std::string ret;
+					std::stringstream ss;
+					ss < data;
+					ss >> ret;
+					return ret;
+				}
+				// serialize reference
+				template<typename T>
+				static std::string serializeReferenceToString(T data)
+				{
+					std::string ret;
+					std::stringstream ss;
+					ss << data->id();
+					ss >> ret;
+					return ret;
+				}
+				// Parse datatype
+				template<typename T>
+				static T parseAttributeFromString(std::string data)
+				{
+					T ret;
+					std::stringstream ss;
+					ss << data;
+					ss >> ret;
+					return ret;
+				}
+				// Parse reference
+				template<typename T>
+				static T parseReferenceFromString(std::string data)
+				{
+					int id;
+					std::stringstream ss;
+					ss << data;
+					ss >> id;
+					return dynamic_cast<T>(modelRoot->findModelElementByID(id));
+				}*/
 			};
 		}
 	}
