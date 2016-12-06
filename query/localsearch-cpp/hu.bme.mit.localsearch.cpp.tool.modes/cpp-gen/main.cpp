921ea91d27d78ba4d21639cd88fe6be29d5db6ee
toth-krisztian <tht.krisztian@gmail.com>
2016-12-06 15:13:38 +0100

    Meres1

--- query/localsearch-cpp/hu.bme.mit.localsearch.cpp.tool.modes/cpp-gen/main.cpp
+++ query/localsearch-cpp/hu.bme.mit.localsearch.cpp.tool.modes/cpp-gen/main.cpp
@@ -1,17 +1,13 @@
 #include "sys/types.h"
 #include "sys/sysinfo.h"
 
-#include "Viatra/Query/DerivedInput/FrozenStateAPIInputUpdater.h"
-#include "Viatra/Query/DistributedQueries/IsDangerousMatcher.h"
+//#include "Viatra/Query/DerivedInput/FrozenStateAPIInputUpdater.h"
+//#include "Viatra/Query/DistributedQueries/IsDangerousMatcher.h"
 #include "Viatra/Query/DistributedQueries/ShouldCollideMatcher.h"
 
 #include "railRoadModel/Segment.h"
 
 #include "Viatra/Query/QueryEngine.h"
-
-//#include"Viatra/Query/Util/Logger.h"
-//#include"Viatra/Query/Distributed/QueryService.h"
-//#include"Viatra/Query/DistributedQueries/QueryRunnerFactory.h"
 
 #include <cstdlib>
 #include "string.h"
@@ -19,17 +15,13 @@
 #include <chrono>
 #include <iomanip>
 #include <thread>
-//#include<windows.h>
 
-//#include "UpdateModel.h"
 using namespace railRoadModel;
 using namespace Viatra::Query;
 using namespace Viatra::Query::DistributedQueries;
 using ModelRoot = Viatra::Query::ModelRoot;
 
 constexpr int test_count = 20;
-//constexpr double update_freq = 100;	// 10 millisec
-//constexpr double update_period = 1.0 / update_freq;
 
 struct Record{
 	double checkTime;
@@ -49,7 +41,7 @@ Storage model;
 std::vector<Record> data;
 
 QueryEngine<ModelRoot> engine = QueryEngine<ModelRoot>::empty();
-IsDangerousQuerySpecification<ModelRoot>::Matcher dangerousMatcher = engine.matcher<IsDangerousQuerySpecification>();
+//IsDangerousQuerySpecification<ModelRoot>::Matcher dangerousMatcher = engine.matcher<IsDangerousQuerySpecification>();
 ShouldCollideQuerySpecification<ModelRoot>::Matcher collideMatcher = engine.matcher<ShouldCollideQuerySpecification>();
 
 auto start_time = std::chrono::high_resolution_clock::now();
@@ -63,10 +55,10 @@ double seconds() {
 	return nanos() / (1000 * 1000 * 1000 * 1.0);
 }
 
-void memory();
-
 void CheckSystemState()
 {
+	//std::cout << "Dangerous: " << dangerousMatcher.matches().size() << " #." <<std::endl;
+	//std::cout << "Collide: " << collideMatcher.matches().size() << " #." <<std::endl;
 	//dangerousMatcher.matches();
 	collideMatcher.matches();
 }
@@ -80,7 +72,6 @@ void runCheckingThread() {
 		auto time = seconds();
 		data.push_back({time - lastT});
 		lastT = time;
-		//memory();
 	}
 }
 
@@ -121,8 +112,8 @@ void init(){
 	auto s11 = new Segment(); s11->id = 11; model.segments.push_back(s11);
 
 	//Modell elemek - Vonatok
-	auto t1001 = new Train(); t1001->id = 1001; model.trains.push_back(t1001);
-	auto t1002 = new Train(); t1002->id = 1002; model.trains.push_back(t1002);
+	auto t1001 = new Train(); t1001->id = 1001; t1001->currentlyOn = s4; model.trains.push_back(t1001);
+	auto t1002 = new Train(); t1002->id = 1002; t1002->currentlyOn = s15; model.trains.push_back(t1002);
 
 	//Modell elemek - Váltók
 	auto turn14 = new Turnout(); turn14->id = 14; turn14->currentState = model.operational; model.turnouts.push_back(turn14);
@@ -189,6 +180,9 @@ void init(){
 	s19->connectedTo.push_back(s7); s19->connectedTo.push_back(turn32);
 	s30->connectedTo.push_back(turn32); s30->connectedTo.push_back(turn28);
 	s11->connectedTo.push_back(turn9); s11->connectedTo.push_back(turn14);
+	
+	std::cout << "Model Size: " << model.robotparts.size() + 
+	model.robots.size() + model.segments.size() + model.trains.size() + model.turnouts.size() + 2 << std::endl;
 
 }
 
@@ -203,108 +197,93 @@ void destruct(){
 	model.robots.clear();
 	for(auto rp : model.robotparts) delete rp;
 	model.robotparts.clear();
+	delete model.frozen;
+	delete model.operational;
 }
-
-int parseLine(char* line){
-    // This assumes that a digit will be found and the line ends in " Kb".
-    int i = strlen(line);
-    const char* p = line;
-    while (*p <'0' || *p > '9') p++;
-    line[i-3] = '\0';
-    i = atoi(p);
-    return i;
-}
-
-int usedVirtual(){ //Note: this value is in KB!
-    FILE* file = fopen("/proc/self/status", "r");
-    int result = -1;
-    char line[128];
-
-    while (fgets(line, 128, file) != NULL){
-        if (strncmp(line, "VmSize:", 7) == 0){
-            result = parseLine(line);
-            break;
-        }
-    }
-    fclose(file);
-    return result;
-}
-
-int usedPhysical(){ //Note: this value is in KB!
-    FILE* file = fopen("/proc/self/status", "r");
-    int result = -1;
-    char line[128];
-
-    while (fgets(line, 128, file) != NULL){
-        if (strncmp(line, "VmRSS:", 6) == 0){
-            result = parseLine(line);
-            break;
-        }
-    }
-    fclose(file);
-    return result;
-}
-
-void memory(){
-	struct sysinfo memInfo;
-	sysinfo (&memInfo);
-	
-	long long totalVirtualMem = memInfo.totalram;
-	//Add other values in next statement to avoid int overflow on right hand side...
-	totalVirtualMem += memInfo.totalswap;
-	totalVirtualMem *= memInfo.mem_unit;
-	
-	long long virtualMemUsed = memInfo.totalram - memInfo.freeram;
-	//Add other values in next statement to avoid int overflow on right hand side...
-	virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
-	virtualMemUsed *= memInfo.mem_unit;
-	
-	long long totalPhysMem = memInfo.totalram;
-	//Multiply in next statement to avoid int overflow on right hand side...
-	totalPhysMem *= memInfo.mem_unit;
-	
-	long long physMemUsed = memInfo.totalram - memInfo.freeram;
-	//Multiply in next statement to avoid int overflow on right hand side...
-	physMemUsed *= memInfo.mem_unit;
-	
-	std::cout << "total virtual memory;total used virtual memory;used virtual memory by process;" 
-		<< "total physical memory;total used physical memory;used physical by process" << std::endl;
-	std::cout <<totalVirtualMem << ";" << virtualMemUsed << ";" << usedVirtual() <<";" 
-		<< totalPhysMem << ";" << physMemUsed << ";" << usedPhysical() << ";" << std::endl;
-}
+//
+//int parseLine(char* line){
+//    // This assumes that a digit will be found and the line ends in " Kb".
+//    int i = strlen(line);
+//    const char* p = line;
+//    while (*p <'0' || *p > '9') p++;
+//    line[i-3] = '\0';
+//    i = atoi(p);
+//    return i;
+//}
+//
+//int usedVirtual(){ //Note: this value is in KB!
+//    FILE* file = fopen("/proc/self/status", "r");
+//    int result = -1;
+//    char line[128];
+//
+//    while (fgets(line, 128, file) != NULL){
+//        if (strncmp(line, "VmSize:", 7) == 0){
+//            result = parseLine(line);
+//            break;
+//        }
+//    }
+//    fclose(file);
+//    return result;
+//}
+//
+//int usedPhysical(){ //Note: this value is in KB!
+//    FILE* file = fopen("/proc/self/status", "r");
+//    int result = -1;
+//    char line[128];
+//
+//    while (fgets(line, 128, file) != NULL){
+//        if (strncmp(line, "VmRSS:", 6) == 0){
+//            result = parseLine(line);
+//            break;
+//        }
+//    }
+//    fclose(file);
+//    return result;
+//}
+//
+//void memory(){
+//	struct sysinfo memInfo;
+//	sysinfo (&memInfo);
+//	
+//	long long totalVirtualMem = memInfo.totalram;
+//	//Add other values in next statement to avoid int overflow on right hand side...
+//	totalVirtualMem += memInfo.totalswap;
+//	totalVirtualMem *= memInfo.mem_unit;
+//	
+//	long long virtualMemUsed = memInfo.totalram - memInfo.freeram;
+//	//Add other values in next statement to avoid int overflow on right hand side...
+//	virtualMemUsed += memInfo.totalswap - memInfo.freeswap;
+//	virtualMemUsed *= memInfo.mem_unit;
+//	
+//	long long totalPhysMem = memInfo.totalram;
+//	//Multiply in next statement to avoid int overflow on right hand side...
+//	totalPhysMem *= memInfo.mem_unit;
+//	
+//	long long physMemUsed = memInfo.totalram - memInfo.freeram;
+//	//Multiply in next statement to avoid int overflow on right hand side...
+//	physMemUsed *= memInfo.mem_unit;
+//	
+//	std::cout << "total virtual memory;total used virtual memory;used virtual memory by process;" 
+//		<< "total physical memory;total used physical memory;used physical by process" << std::endl;
+//	std::cout <<totalVirtualMem << ";" << virtualMemUsed << ";" << usedVirtual() <<";" 
+//		<< totalPhysMem << ";" << physMemUsed << ";" << usedPhysical() << ";" << std::endl;
+//}
 
 int main(int argc, char**argv)
 {
 	data.reserve(test_count);
-	//const char * nodeName = argc > 1 ? argv[1] : "nodeA";
-	// Creating the Local Model from the image
-	//memory();
 	std::cout <<"Starting init - done"<< std::endl;
 	init();
 	std::cout <<"Ending init - done"<< std::endl;
-	
-	std::cout << "Start" << std::endl;
-	Viatra::Query::ModelRoot modelRoot;
 
 	std::cout <<"Starting measure - done"<< std::endl;
 
 	try {
-
-		//std::cout <<"try - runUpdatingThread"<< std::endl;
-		//runUpdatingThread(nodeName, &modelRoot);
-
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-
-		std::cout <<"try - runCheckingThread"<< std::endl;
 		runCheckingThread();
-		memory();
-
-		std::cout <<"try - for"<< std::endl;
 		for (auto & record : data)
 		{
-			std::cout << std::fixed << std::setprecision(10) << record.checkTime << ";";
+			std::cout << std::fixed << std::setprecision(12) << record.checkTime << ";";
 		}
-
 	}
 	catch (const char * c)
 	{
